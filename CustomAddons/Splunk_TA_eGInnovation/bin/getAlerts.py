df8e374a36f3cbb61ca5fe063f6baf217fd6d545
cchsing <jcch950607@gmail.com>
2022-09-13 15:05:05 +0800

    adding in files for Add-on

---
+++ CustomAddons/Splunk_TA_eGInnovation/bin/getAlerts.py
@@ -0,0 +1,31 @@
+from __future__ import print_function
+
+from configs import HeadersPars, getAlerts
+
+import sys, requests, json, time
+
+def main():
+    # define api endpoint
+    API_ENDPOINT = HeadersPars['managerurl'] + getAlerts['rscPath']
+    # print (API_ENDPOINT)
+
+    # header for the request
+    REQHEADER = HeadersPars
+    REQHEADER['Content-Type'] = getAlerts['Content-Type']
+    # print (reqHeader)
+
+    # body for the request
+    REQDATA = getAlerts['Body']
+    # print (reqData)
+
+    # http post request
+    try: 
+        # DATA = requests.get(API_ENDPOINT)
+        DATA = requests.post(API_ENDPOINT, headers=REQHEADER, json=REQDATA)
+        DATA_DICT = json.loads(DATA.text)
+        sys.stdout.write(json.dumps(DATA_DICT, indent=3))
+    except:
+        sys.stderr.write("POST Request to getAlerts endpoint error. ")
+        
+if __name__ == "__main__":
+    main()
---
+++ CustomAddons/Splunk_TA_eGInnovation/bin/showtestsdetails.py
@@ -0,0 +1,31 @@
+from __future__ import print_function
+
+from configs import HeadersPars, showtestsdetails
+
+import sys, requests, json, time
+
+def main():
+    # define api endpoint
+    API_ENDPOINT = HeadersPars['managerurl'] + showtestsdetails['rscPath']
+    # print (API_ENDPOINT)
+
+    # header for the request
+    REQHEADER = HeadersPars
+    REQHEADER['Content-Type'] = showtestsdetails['Content-Type']
+    # print (REQHEADER)
+
+    # body for the request
+    REQDATA = showtestsdetails['Body']
+    # print (REQDATA)
+
+    # http post request
+    try:
+        DATA = requests.post(API_ENDPOINT, headers=REQHEADER, json=REQDATA)
+        DATA_DICT = json.loads(DATA.text)
+        sys.stdout.write(json.dumps(DATA_DICT, indent=3))
+    except:
+        sys.stderr.write("POST Request to showtestsdetails endpoint error.")
+
+
+if __name__ == "__main__":
+    main()
