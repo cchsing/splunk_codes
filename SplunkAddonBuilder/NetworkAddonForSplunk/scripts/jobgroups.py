f69cbc8f6b71cd2765ea9875fcd9d3dabb532ab6
cchsing <jcch950607@gmail.com>
2022-12-20 19:07:47 +0800

    Update jobgroups.py

--- SplunkAddonBuilder/NetworkAddonForSplunk/scripts/jobgroups.py
+++ SplunkAddonBuilder/NetworkAddonForSplunk/scripts/jobgroups.py
@@ -3,9 +3,16 @@ import sys
 import time
 import datetime
 import json
+import re
+import traceback
+
 
 def validate_input(helper, definition):
     pass
 
+
 def collect_events(helper, ew):
-    
+    opt_hostname_ip_address = helper.get_arg('hostname_ip_address')
+    opt_port = helper.get_arg('port')
+
+    URL1 = "https://"
