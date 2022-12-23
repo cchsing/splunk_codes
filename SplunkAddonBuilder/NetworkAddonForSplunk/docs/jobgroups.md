d1778e7a48b154e93722b306f89d5af60fd51ad8
Justin Chong, Chia-Hsing <chiahsing.chong@outlook.com>
2022-12-23 12:54:08 +0800

    updates scripts

--- SplunkAddonBuilder/NetworkAddonForSplunk/docs/jobgroups.md
+++
@@ -1,2 +0,0 @@
-# The jobgroups endpoint
-
--- SplunkAddonBuilder/NetworkAddonForSplunk/scripts/jobgroups.py
+++ SplunkAddonBuilder/NetworkAddonForSplunk/scripts/jobgroups.py
@@ -5,7 +5,11 @@ import datetime
 import json
 import re
 import traceback
+import base64
 
+def basic_auth(username, password):
+    token = base64.b64encode(bytes(f"{username}:{password}","utf-8")).decode("ascii")
+    return f'Basic {token}'
 
 def validate_input(helper, definition):
     pass
